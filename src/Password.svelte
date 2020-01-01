2d2204889f16d78ef3bc0efd9558b3b747356727
Boris Panchenko <oberon@list.ru>
2020-01-01 17:46:52 +0300

    select password by click, resolve issue #3

--- src/Password.svelte
+++ src/Password.svelte
@@ -1,8 +1,25 @@
 <script>
     export let value
+    let container
+
+    function selectNode(node) {
+        if (document.body.createTextRange) {
+            const range = document.body.createTextRange()
+            range.moveToElementText(node)
+            range.select()
+        } else if (window.getSelection) {
+            const selection = window.getSelection()
+            const range = document.createRange()
+            range.selectNodeContents(node)
+            selection.removeAllRanges()
+            selection.addRange(range)
+        } else {
+            console.warn("Could not select text in node: Unsupported browser.")
+        }
+    }
 </script>
 
-<h2 class="o-password" style="min-width: {value.length}ch;">
+<h2 bind:this={container} on:click|preventDefault={() => selectNode(container)} class="o-password" style="min-width: {value.length}ch;">
     {value}
 </h2>
 
